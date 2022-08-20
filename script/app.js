94f7c39de4a2e846ec504cc9c56cd1bb103a6d44
rekky1aws <killikomfr@gmail.com>
2022-08-21 01:59:29 +0200

    Added more intuitive display when moving items.

--- script/app.js
+++ script/app.js
@@ -1,6 +1,6 @@
 // Classes
 class Game {
-    constructor (name = 'No Game', imageLink = 'media/images/game-placeholder.jpg') {
+    constructor (name = 'No Game', imageLink = null) {
         this.name = name;
         this.imageLink = imageLink;
     }
@@ -8,10 +8,15 @@ class Game {
     display () {
         let game = document.createElement('div');
         game.className="game hover-light";
-        let image = document.createElement('img');
+        let image;
+        if (this.imageLink !== null) {
+            image = document.createElement('img');
+            image.alt = this.name;
+            image.src = this.imageLink; 
+        } else {
+            image = document.createElement('div');
+        }
         image.className = 'game-image';
-        image.alt = this.name;
-        image.src = this.imageLink;
         game.appendChild(image);
         return game;
     }
@@ -71,22 +76,50 @@ function swapItems (sourceA, sourceB)
     }
 }
 
+function placeImageOnCursor()
+{
+    movingImage.style.left = `${cursPos.x + 5}px`;
+    movingImage.style.top = `${cursPos.y + 5}px`;
+}
+
 // Main
 games.displayGames;
 
-let selectedItem = null;
+let selectedItem;
+const cursPos = {x: undefined, y: undefined};
+let grabbingInterval;
+let movingImage;
 
 document.body.addEventListener('mousedown', (e) => {
     if (e.target.className.split(' ').indexOf('game-image') !== -1) {
         selectedItem = e.target;
         document.body.style.cursor = 'grabbing';
+        if (selectedItem.src) {
+            movingImage = document.createElement('img');
+            movingImage.src = selectedItem.src;
+            movingImage.className = 'moving-image';
+        } else {
+            movingImage = document.createElement('div');
+            movingImage.className = 'moving-image empty-moving-image';
+        }
+        document.body.appendChild(movingImage);
+        grabbingInterval = setInterval(placeImageOnCursor, 10);
     }
+});
+
+document.body.addEventListener('mousemove', (e) => {
+    cursPos.x = e.x;
+    cursPos.y = e.y;
 });
 
 document.body.addEventListener('mouseup', (e) => {
     document.body.removeAttribute('style');
     if (e.target.className.split(' ').indexOf('game-image') !== -1 && selectedItem) {
+        document.body.removeChild(movingImage);
         swapItems(e.target, selectedItem);
+        clearInterval(grabbingInterval);
+        cursPos.x = null;
+        cursPos.y = null;
     }
     selectedItem = null;
 
