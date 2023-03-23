a1ee00a6926749dd74989f0d7116f18b0687c93f
rekky1aws <killikomfr@gmail.com>
2023-03-23 19:39:48 +0100

    Comments for main menu's app.js

--- script/app.js
+++ script/app.js
@@ -7,29 +7,42 @@ class Game {
     }
 
     display () {
+        // Variables
         let game = document.createElement('a');
+        let image;
+
+        // Ajout des propriétés à l'element
         game.href = this.gameLink;
         game.draggable = "false";
         game.className="game hover-light";
-        let image;
+
+        // On crée une image si on a une app
         if (this.imageLink !== null) {
             image = document.createElement('img');
             image.alt = this.name;
             image.src = this.imageLink;
+        // Sinon on crée une div
         } else {
             image = document.createElement('div');
         }
+
+        // Propriétés de l'element
         image.className = 'game-image';
         image.draggable = "false";
+
         game.appendChild(image);
+        
         return game;
     }
 }
 
 class GameCollection {
+    // Une GameCollection représente une grille de jeux sur un écran.
+    // Elle permet de gérer automatiquement le fait que les jeux s'affichent par grilles de 3*4
     constructor (games = [])
     {
         this.collection = [];
+
         for (let i = 0; i < games.length + (12 - (games.length % 12)); i++) {
             if (i < games.length) {
                 this.collection.push(games[i]);
@@ -42,11 +55,14 @@ class GameCollection {
     get displayGames ()
     {
         const mainElement = document.querySelector('main');
+
         for (let i = 0; i < Math.floor(this.collection.length / 12); i++) {
             let sectionElement = document.createElement('section');
+            
             for (let j = i*12; j < 12 * (i + 1); j++) {
                 sectionElement.appendChild(this.collection[j].display());
             }
+
             mainElement.appendChild(sectionElement);
         }
     }
@@ -65,6 +81,7 @@ const games = new GameCollection (
 // Fonctions
 function swapItems (sourceA, sourceB)
 {
+    // Permet de procéder à l'échange entre deux app
     if (sourceA !== sourceB)
     {
         // Getting parents
@@ -83,6 +100,7 @@ function swapItems (sourceA, sourceB)
 
 function placeImageOnCursor()
 {
+    // Permet de faire suivre l'image du curseur wii par rapport au curseur réel
     movingImage.style.left = `${cursPos.x + 5}px`;
     movingImage.style.top = `${cursPos.y + 5}px`;
 }
@@ -90,42 +108,65 @@ function placeImageOnCursor()
 // Main
 games.displayGames;
 
+// Variables Globales
 let selectedItem;
 let grabbingInterval;
 let movingImage;
 
+// Detection du début du clic de la souris
 document.body.addEventListener('mousedown', (e) => {
+    // On vérifie si l'endoroit cliqué est une image d'app
     if (e.target.className.split(' ').indexOf('game-image') !== -1) {
+        
         selectedItem = e.target;
+        
+        // Si on a une src pour une image, on crée un dupliqué de l'image qui va suivre la souris jusqu'au relachement 
         if (selectedItem.src) {
             movingImage = document.createElement('img');
             movingImage.src = selectedItem.src;
             movingImage.className = 'moving-image';
-              cursorImage.src = "media/images/cursors/grabbing_cursor.png";
+            cursorImage.src = "media/images/cursors/grabbing_cursor.png";
+        // Sinon on crée une div qui aura le même comportement, avec le fond par défaut
         } else {
             movingImage = document.createElement('div');
             movingImage.className = 'moving-image empty-moving-image';
         }
+
+        // On ajoute l'image au body pour qu'elle soit visible
         document.body.appendChild(movingImage);
+
+        // On crée un intervale qui va déplacer l'image toutes les 10ms pour qu'elle suive le curseur
         grabbingInterval = setInterval(placeImageOnCursor, 10);
     }
 });
 
+// Detection de la fin du clic de la souris
 document.body.addEventListener('mouseup', (e) => {
+    // On reset le style inline du body
     document.body.removeAttribute('style');
+    
+    // On vérifie la nature de l'endroit ou l'on arrete le clic
     if (e.target.className.split(' ').indexOf('game-image') !== -1 && selectedItem) {
+        // Echange des images et des liens des apps
         swapItems(e.target, selectedItem);
+        
+        // Suppression de l'intervalle pour le déplacement de la copie d'image
         clearInterval(grabbingInterval);
+
         cursPos.x = null;
         cursPos.y = null;
     }
+
     selectedItem = null;
+    
+    // Suppression de la copie d'image
     if (movingImage && movingImage.parentNode) {
         movingImage.parentNode.removeChild(movingImage);
     }
 
 });
 
+// Changement visuel du curseur lorsqu'on le passe sur un emplcement d'app
 document.querySelectorAll('.game-image').forEach(element => {
     element.addEventListener('mouseenter', () => {
         if (selectedItem === null || selectedItem === undefined) {
