856e591b324c23ba7feee447a56ec0d4cbe894e5
rekky1aws <killikomfr@gmail.com>
2025-04-07 14:11:36 +0200

    [MCA] Player entries
    At first there is only one input field. When user types in it automatically
    adds a new field. When there are more than two players and a field is
    emptied, it is automatically deleted.

---
+++ mister-coco/script/mister-coco.js
@@ -0,0 +1,31 @@
+// CONSTANTS
+const playerNameElts = document.querySelectorAll('.player-name');
+
+// VARIABLES
+
+// FUNCTIONS
+function playerNameAction (evt) {
+	if (evt.target.value === "" && evt.target.parentNode.children.length > 2) {
+		console.log(evt.target.value);
+		evt.target.remove();
+	}
+
+	if (evt.target.value !== "" && evt.target.parentNode.lastElementChild.value !== "") {
+
+		const newInput = document.createElement('input');
+		newInput.type = "text";
+		newInput.name = "player-name";
+		newInput.placeholder = "Enter player name here";
+		newInput.classList.add("player-name");
+
+		newInput.addEventListener('keyup', playerNameAction);
+		evt.target.parentNode.append(newInput);
+	}
+}
+
+// EVENT LISTENERS
+playerNameElts.forEach((elt) => {
+	elt.addEventListener("keyup", playerNameAction);
+});
+
+// MAIN
