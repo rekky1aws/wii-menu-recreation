0b54f314027e1bfd3b20df3de4e04acc6389bd30
rekky1aws <killikomfr@gmail.com>
2025-07-19 16:50:21 +0200

    [CAL] Btn handling init

--- calculator/script/calculator.js
+++ calculator/script/calculator.js
@@ -1,1 +1,17 @@
-console.log('Hello Calculator');
+// CONSTANTS
+const keys = document.querySelectorAll('.key');
+
+// VARIABLES
+
+// FUNCTION
+function btnHandler (evt)
+{
+	console.log(evt.target.textContent);
+}
+
+// EVENT LISTENERS
+keys.forEach((e) => {
+	e.addEventListener("click", btnHandler);
+});
+
+// MAIN
