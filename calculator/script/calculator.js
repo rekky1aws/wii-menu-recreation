827ec879f06710413a405949c2e8101c289f2d08
rekky1aws <killikomfr@gmail.com>
2025-07-20 14:05:19 +0200

    JS Btn handling structure

--- calculator/script/calculator.js
+++ calculator/script/calculator.js
@@ -1,12 +1,50 @@
 // CONSTANTS
 const keys = document.querySelectorAll('.key');
+const expZone = document.querySelector('#exp');
 
 // VARIABLES
 
 // FUNCTION
 function btnHandler (evt)
 {
-	console.log(evt.target.textContent);
+	if (evt.target.id) {
+		console.log(evt.target.id);
+
+		switch(evt.target.id)
+		{
+			case "back-key":
+
+				break;
+
+			case "equal-key":
+
+				break;
+
+			case "mod-key":
+
+				break;
+
+			case "pi-key":
+			
+				break;
+
+			case "sqrt-key":
+
+				break;
+
+			case "percent-key":
+
+				break;
+
+			case "sq-key":
+
+				break;
+		}
+
+		return false;
+	}
+
+	expZone.value = expZone.value + evt.target.textContent
 }
 
 // EVENT LISTENERS
