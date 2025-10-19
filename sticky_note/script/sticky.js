5eebace7419568c82d88dbd672022c5fc8ad80ab
rekky1aws <killikomfr@gmail.com>
2025-10-19 08:36:40 +0200

    [STI] Date and Countdown display
    Hid Date and added static Countdown display.
    TODO :
     + Make the countdown display refresh every second
     + Create a list of task where the time limit is over
     + Check if task's time limit is over (and add it to the list if it is
       the case).
     + On load display a menu to choose which timed out tasks to  keep (list
       of checkboxes)

--- sticky_note/script/sticky.js
+++ sticky_note/script/sticky.js
@@ -10,24 +10,60 @@ function deleteNote (evt)
   }
 }
 
-function createNewNoteElt (content)
+function refreshCD (container)
 {
+
+}
+
+function createNewNoteElt (content, date='')
+{
+  // Creating HTML tags.
   const newNoteElt = document.createElement('li');
   const newNoteContent = document.createElement('div');
   const newNoteDate = document.createElement('div');
+  const newNoteCD = document.createElement('div');
   const newNoteDelBtn = document.createElement('button');
 
+  // HTML tags classes
   newNoteElt.classList.add('note');
   newNoteContent.classList.add('note-content');
   newNoteDelBtn.classList.add('clickable', 'note-del');
-  newNoteDate.classList.add('note-date');
+  newNoteDate.classList.add('note-date', 'hidden');
+  newNoteCD.classList.add('note-countdown');
 
+  // Handling dates
+  let noteDate;
+  if (date) { // If task already exists and has a date
+    noteDate = new Date(date);
+  } else { // Else add date to 24h after creation
+    noteDate = new Date(new Date().getTime() + (60 * 60 * 24 * 1000));
+  }
+  const countdownDate = new Date(noteDate.getTime() - new Date().getTime());
+  // Pre-pending a 0 if the number is only one character long
+  let countdownHours = `${countdownDate.getTime() / (60 * 60 * 1000)}`;
+  if (countdownHours.length == 1) {
+    countdownHours = "0" + countdownHours;
+  }
+  let countdownMinutes = `${(countdownDate.getTime() / (60 * 1000)) % 60}`;
+  if (countdownMinutes.length == 1) {
+    countdownMinutes = "0" + countdownMinutes;
+  }
+  let countdownSeconds = `${(countdownDate.getTime() / (1000)) % 60}`;
+  console.log(countdownSeconds.length);
+  if (countdownSeconds.length == 1) {
+    countdownSeconds = "0" + countdownSeconds;
+  }
+
+  // Setting HTML tags content
   newNoteContent.textContent = content;
-  newNoteDate.textContent = new Date().toString()
+  newNoteDate.textContent = noteDate.toISOString();
+  newNoteCD.textContent = countdownHours + ":" + countdownMinutes + ":" + countdownSeconds;
 
+  // Deletion button interaction
   newNoteDelBtn.addEventListener('click', deleteNote);
 
-  newNoteElt.append(newNoteContent, newNoteDate, newNoteDelBtn);
+  // Adding tags to the DOM
+  newNoteElt.append(newNoteContent, newNoteDate, newNoteCD, newNoteDelBtn);
   notesContainer.append(newNoteElt);
 }
 
@@ -37,6 +73,7 @@ function newNoteHandler (evt)
   
   if (evt.key == "Enter") {
     createNewNoteElt(newNote.value);
+    // saveNotes();
     newNote.value = "";
   }
 }
